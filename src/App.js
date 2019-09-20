f59d6c8422fc443812f868b89f1f02a141d2aba9
froggagul <froggagul@gmail.com>
2019-09-20 22:12:13 +0900

    20190920

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+
+function useWindowMousePosition() {
+  let [WindowMousePosition, setWindowMousePosition] = useState({
+    x: null,
+    y: null
+  });
+
+  function handleMouseMove(e) {
+    setWindowMousePosition({
+      x: e.pageX,
+      y: e.pageY
+    });
+  }
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  });
+
+  return WindowMousePosition;
+}
 
 function App() {
+  //State Hook
+  const [count, setCount] = useState(0);
+  
+
+  //Effect Hook
+  useEffect(()=> {
+    document.title=`You clicked ${count} times`;
+  },[count]);
+  
+  let { x, y } = useWindowMousePosition();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <p>You clicked {count}times</p>
+        <button onClick={() => setCount(count+1)}>
+          Clickme
+        </button>
+        <div style={{width : "100%", height : "80%" }}>
+          <pre>{JSON.stringify({x,y})}</pre>
+        </div>  
+      </div>
+    );
 }
 
 export default App;
